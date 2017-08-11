cc036aeb9277d0902bd3c67c596a6174933e0b1f
TPei <t.s.peikert@gmail.com>
2017-08-11 11:07:40 +0200

    rename roleable to accessor, leave roleable as alias to accessor

--- src/rbac/roleable.cr
+++ src/rbac/roleable.cr
@@ -1,37 +1,5 @@
 module Rbac
   module Roleable
-    getter roles
-
-    @roles = [] of Symbol
-
-    # add allowed roles
-    # ```
-    # u = User.new # includes Roleable
-    # u.has_roles :add, :delete
-    # ```
-    def has_roles(*_roles : Symbol)
-      @roles += _roles.to_a
-      @roles.uniq!
-    end
-
-    # add allowed roles
-    # ```
-    # s = Store.new # includes Resource
-    # s.has_roles [:add, :delete]
-    # ```
-    def has_roles(roles : Array(Symbol))
-      @roles += roles
-      @roles.uniq!
-    end
-
-    # check if a accessor has a specific role
-    # ```
-    # u = User..new # includes Roleable
-    # u.has_roles :add, :delete
-    # u.has_role? :add # => true
-    # ```
-    def has_role?(role : Symbol)
-      @roles.includes? role
-    end
+    include Accessor
   end
 end
